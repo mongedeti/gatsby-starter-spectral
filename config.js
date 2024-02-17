198867a93a1a94353a60fdd03180ebf6b1c29c89
mongedeti <160082855+mongedeti@users.noreply.github.com>
2024-02-16 23:21:38 -0300

    Update config.js
    Alteração do título e subtítulo da página.

--- config.js
+++ config.js
@@ -8,8 +8,8 @@ module.exports = {
   manifestDisplay: 'standalone',
   manifestIcon: 'src/assets/img/website-icon.png',
   pathPrefix: `/gatsby-starter-spectral/`, // This path is subpath of your hosting https://domain/portfolio
-  heading: 'Anubhav Srivastava',
-  subHeading: 'Full time Web Developer. Part time Open source contributor  ',
+  heading: 'Tecnologia é para todos',
+  subHeading: 'Mergulhe em um mundo de aprendizado, seja você um iniciante ou um entusiasta',
   // social
   socialLinks: [
     {
